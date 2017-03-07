68d1009151406a3f301c53b81f7af8d7459cee45
caravard <kotertom@gmail.com>
2017-03-07 22:35:11 +0100

    Generated classes from schema. Split into folder/namespace structure. Needs revision

---
+++ src/Common/Schema/Message/PlayerMessage/AcceptExchangeRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Schema.Message.PlayerMessage
+{
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType=true, Namespace="http://theprojectgame.mini.pw.edu.pl/")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace="http://theprojectgame.mini.pw.edu.pl/", IsNullable=false)]
+    public partial class AcceptExchangeRequest : BetweenPlayersMessage {
+    }
+}
