65c11f1c91710ffb08a6a17f78c756a5bede2bb1
Kamil Grabowski <grabowskik@student.mini.pw.edu.pl>
2017-03-08 15:09:29 +0100

    Refactor mock game master code into connection class.

---
+++ src/Common/Connection/IConnetion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface IConnetion
+    {
+        void StartClient(int port, string ipString);
+        void ConnectCallback(IAsyncResult ar);
+        void Receive(Socket client);
+        void ReceiveCallback(IAsyncResult ar);
+        void Send(Socket client, String data);
+        void SendCallback(IAsyncResult ar);
+
+
+    }
+}
--- src/Common/Connection/StateObject.cs
+++ src/Common/Connection/StateObject.cs
@@ -5,7 +5,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MockGameMaster
+namespace Common.Connection
 {
     // State object for receiving data from remote device.
     public class StateObject
