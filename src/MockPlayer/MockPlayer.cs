42482a0ca825539489fd5830a3f7486d3f032238
jasinskib <b.jasinski@student.mini.pw.edu.pl>
2017-03-12 21:56:02 +0100

    Player server connection should work

--- src/MockPlayer/MockPlayer.cs
+++ src/MockPlayer/MockPlayer.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommandLine;
+using Common.Connection;
+using MockGameMaster;
 using MockPlayer.IO;
 
 namespace MockPlayer
@@ -12,8 +14,10 @@ namespace MockPlayer
     {
         public static void Main(string[] args)
         {
-            CommandLineOptions options = CommandLineParser.ParseArgs(args);
-            
+         //   CommandLineOptions options = CommandLineParser.ParseArgs(args);
+            AsynchronousClient client = new AsynchronousClient(new Connection("192.168.0.19", 12196));
+            client.Connect();
+            client.Disconnect();
 
         }
     }
