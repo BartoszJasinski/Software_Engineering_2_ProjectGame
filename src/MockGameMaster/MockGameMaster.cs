8945bf40f660cb4639e1ec53cd097cb96ed74400
jasinskib <b.jasinski@student.mini.pw.edu.pl>
2017-03-15 21:22:22 +0100

    Big changes in projects structure

--- src/MockGameMaster/MockGameMaster.cs
+++ src/MockGameMaster/MockGameMaster.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CommandLine;
 using Common.Connection;
 using Common.IO.Console;
 using Common.IO.Net;
 
-namespace MockPlayer
+namespace MockGameMaster
 {
-    public class MockPlayer
+    class MockGameMaster
     {
-        public static void Main(string[] args)
+        //adres post metoda connect
+        static void Main(string[] args)
         {
             CommandLineOptions options = CommandLineParser.ParseArgs(args);
 
             AsynchronousClient client = new AsynchronousClient(new Connection(options.Address, options.Port));
             client.Connect();
             client.Disconnect();
-
         }
     }
 }
