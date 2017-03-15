87ee531e4b837ac3a7c35af6acbe34986aa367aa
grajekf <grajekf>
2017-03-15 15:43:48 +0100

    Added schema files

---
+++ src/Common/Schema/Message/Server/ConfirmGameRegistration.cs
@@ -0,0 +1,25 @@
+﻿namespace Common.Schema.Message.Server
+{
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType=true, Namespace="http://theprojectgame.mini.pw.edu.pl/")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace="http://theprojectgame.mini.pw.edu.pl/", IsNullable=false)]
+    public partial class ConfirmGameRegistration {
+    
+        private ulong gameIdField;
+    
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public ulong gameId {
+            get {
+                return this.gameIdField;
+            }
+            set {
+                this.gameIdField = value;
+            }
+        }
+    }
+}
