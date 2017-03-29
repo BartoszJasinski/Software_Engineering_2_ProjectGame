089685c80c46b220368b976de35ec33bd91acf83
jasinskib <b.jasinski@student.mini.pw.edu.pl>
2017-03-29 06:01:48 +0200

    Stub of connecting to game

---
+++ src/Player/Logic/MakeDecission.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Schema;
+
+namespace Player.Logic
+{
+    //TODO It is temporary class for now we have to decide on Logic architecture in Player and GM. 
+    //imho we should have basic message handling in BehaviourChooser and pass handled data to specific classes in Logic packages
+    class MakeDecission
+    {
+        void RegisteredGames(RegisteredGames registeredGames, PlayerData playerData)
+        {
+            foreach (var game in registeredGames.GameInfo)
+            {
+                if (playerData.prefferedTeam == "blue")
+                    if (game.blueTeamPlayers > 0)
+                        //TODO how we should send this JoinGame message?
+                        //send JoinGame
+                        ;
+                    else ;
+                else ;//.....
+            }
+
+
+        }
+    }
+}
---
+++ src/Player/Logic/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player.Logic
+{
+    //TODO It is temporary class for now we have to decide on Logic architecture in Player and GM.
+    //we have to keep player data somewhere, we should decide how to do that 
+    class PlayerData
+    {
+        public string prefferedTeam { get; set; }
+    }
+}
