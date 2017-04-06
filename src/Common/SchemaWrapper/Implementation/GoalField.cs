f2566259a0177b7aa89ae56e32ab70a737f31010
Filip Grajek <filip.grajek@gmail.com>
2017-04-06 23:19:38 +0200

    Added Field Wrappers

---
+++ src/Common/SchemaWrapper/Implementation/GoalField.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Schema;
+
+namespace Common.SchemaWrapper
+{
+    public class GoalField : Field
+    {
+        private Schema.GoalField goalSchemaField;
+
+        public GoalField() : base()
+        {
+            goalSchemaField = new Schema.GoalField();
+        }
+
+        public override Schema.Field SchemaField
+        {
+            get
+            {
+                return goalSchemaField;
+            }
+
+        }
+
+        public GoalFieldType Type
+        {
+            get { return goalSchemaField.type; }
+            set { goalSchemaField.type = value; }
+        }
+
+        public TeamColour Team
+        {
+            get { return goalSchemaField.team; }
+            set { goalSchemaField.team = value; }
+        }
+    }
+}
