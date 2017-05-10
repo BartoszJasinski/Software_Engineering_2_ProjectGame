a763d82da0144f9600901c11b6dc48d33ecb9157
Kamil <Kamil@LAPTOP-JCEE75RC>
2017-05-10 14:37:03 +0200

    IGame interface

---
+++ src/Player/Net/IGame.cs
@@ -0,0 +1,22 @@
+﻿using Common.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player.Net
+{
+    public interface IGame
+    {
+        void HandleMessage(RegisteredGames message);
+        void HandleMessage(ConfirmJoiningGame message);
+        void HandleMessage(RejectJoiningGame message);
+        void HandleMessage(Game message);
+        void HandleMessage(Data message);
+        void HandleMessage(KnowledgeExchangeRequest message);
+        void HandleMessage(AcceptExchangeRequest message);
+        void HandleMessage(RejectKnowledgeExchange message);
+        void HandleMessage(object message);
+    }
+}
